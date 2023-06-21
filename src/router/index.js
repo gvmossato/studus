eaab01dec87d0ce711f182c531127cd1ba1e3aae
gvmossato_loyall <gabriel@loyall.com.br>
2023-06-21 16:21:40 -0300

    add dynamic page titles

--- src/router/index.js
+++ src/router/index.js
@@ -9,31 +9,37 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView,
+    meta: { title: 'studus | Início' },
   },
   {
     path: '/about',
     name: 'about',
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    meta: { title: 'studus | Sobre' },
   },
   {
     path: '/mailing',
     name: 'mailing',
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    meta: { title: 'studus | Mailing' },
   },
   {
     path: '/free',
     name: 'free',
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    meta: { title: 'studus | Gratuito' },
   },
   {
     path: '/friend',
     name: 'friend',
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    meta: { title: 'studus | Amigo' },
   },
   {
     path: '/guide',
     name: 'guide',
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    meta: { title: 'studus | Guia' },
   },
 ];
 
